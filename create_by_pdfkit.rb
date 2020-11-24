9606f5d0e9dbcd74ac9a74271f6ce338cba3a5c5
Shuhei Kinukawa <kinushu@gmail.com>
2020-11-24 22:26:13 +0900

    [wip] まだ日本語表示がうまくいっていない

--- create_by_pdfkit.rb
+++ create_by_pdfkit.rb
@@ -1,5 +1,17 @@
 require 'pdfkit'
 
+PDFKit.configure do |config|
+  config.default_options = {
+    :encoding=>"utf-8",
+    :page_size=>"A4",
+    :margin_top=>"0.25in",
+    :margin_right=>"0.25in",
+    :margin_bottom=>"0.25in",
+    :margin_left=>"0.25in",
+    :disable_smart_shrinking=>false
+  }
+end
+
 def main_proc
   kit = PDFKit.new('http://google.com')
   file = kit.to_file('tmp/testout_pk.pdf')
